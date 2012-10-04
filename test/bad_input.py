4e516bfc9a5ff2ffb2f6a72c36adea7502c5ecb4
Douglas Anderson <dougjamesanderson@gmail.com>
2012-10-03 20:39:50 -0400

    Added bad_input test. Plans to redo tests

---
+++ test/bad_input.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+import subprocess
+import time
+
+PROJECTDIR =  os.path.abspath( os.path.join(os.path.dirname( os.path.realpath(__file__)), os.path.pardir)) + "/"
+ 
+SOLVER = PROJECTDIR + "/bin/solver"
+INPUT = PROJECTDIR + "test/input/bad_input.txt"
+LOG = PROJECTDIR + "test/log/once.txt"
+
+r = open(INPUT, 'r')
+l = open(LOG, 'w')
+n = open("/dev/null", 'w')
+
+i = 0
+line = r.readline()
+start = time.time()
+while line:
+    i += 1
+    l.write("game:" + str(i))
+    before = time.time()
+    solved = subprocess.call([SOLVER, line])
+    after = time.time()
+    l.write("\tattempt: " + str(after - before))
+    l.write("\tsolved:" + str(solved) + "\n")
+    l.flush()
+    line = r.readline()
+end = time.time()
+l.write("Total Duration:" + str(end - start) + "\n")
+
+r.close()
+l.close()
+n.close()
